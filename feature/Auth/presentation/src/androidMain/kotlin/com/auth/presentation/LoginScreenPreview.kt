9f821b6c58f57657095773ef347779265fc5b133
mitulsheth <iammitulsheth@gmail.com>
2025-12-29 23:03:43 +0530

    Preview Fix and Navigation 3 Setup

---
+++ feature/Auth/presentation/src/androidMain/kotlin/com/auth/presentation/LoginScreenPreview.kt
@@ -0,0 +1,22 @@
+package com.auth.presentation
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+/*@Preview(showBackground = true, name = "Light Mode")
+@Composable
+fun LoginScreenLightPreview() {
+    MaterialTheme(colorScheme = lightColorScheme()) {
+        LoginScreen()
+    }
+}
+
+@Preview(showBackground = true, name = "Dark Mode")
+@Composable
+fun LoginScreenDarkPreview() {
+    MaterialTheme(colorScheme = darkColorScheme()) {
+        LoginScreen()
+    }
+}*/
---
+++ feature/Auth/presentation/src/commonMain/kotlin/com/auth/presentation/login/LoginScreen.kt
@@ -0,0 +1,177 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.auth.presentation.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kmp.designsystem.theme.AppTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen() {
+    Surface {
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Welcome to WorldWideAuto",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(
+                text = "Sign in or create an account to get started.",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 8.dp, bottom = 32.dp)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            var email by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email address") },
+                leadingIcon = { Icon(Icons.Filled.Email, contentDescription = "Email Icon") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            var password by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                leadingIcon = { Icon(Icons.Filled.Lock, contentDescription = "Password Icon") },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Text(
+                text = "Forgot Password?",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { /* TODO: Handle Forgot Password */ }
+                    .padding(top = 16.dp),
+                textAlign = TextAlign.End,
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(50),
+                    modifier = Modifier.weight(1f).padding(end = 8.dp)
+                ) {
+                    Text("Sign Up")
+                }
+                Button(
+                    onClick = {  },
+                    shape = RoundedCornerShape(50),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+                    modifier = Modifier.weight(1f).padding(start = 8.dp)
+                ) {
+                    Text("Sign In")
+                }
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Divider(modifier = Modifier.weight(1f))
+                Text(
+                    text = "OR",
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    fontSize = 14.sp
+                )
+                Divider(modifier = Modifier.weight(1f))
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            AppleSignInButton(
+                onClick = {}
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            GoogleSignInButton(
+                onClick = {}
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            PhoneSignInButton(onClick = {})
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "By continuing, you agree to our Terms and Privacy Policy.",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreviewLight() {
+    AppTheme { LoginScreen() }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreviewDark() {
+    AppTheme(darkTheme = true) { LoginScreen() }
+}
